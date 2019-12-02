fc8e177393f67e1f5f4c225cfec078c731c8d745
Shishir Jha <shishirjhaccr@gmail.com>
2019-12-02 14:56:50 +0000

    created new folder for day to day problems

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
 # Playground
-l = []
-
-with open('input.txt') as inp:
-  for i in inp:
-    l.append(int(i))
-solution = 0
-for i in l:
-  while (i//3)-2 >= 0:
-    i = i//3
-    i-=2
-    solution+=i
-print(solution)
